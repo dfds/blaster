22ea14d2a4cdfbb7d56b408f7624524306f526f2
Jens Andresen <jandr@DFDS.COM>
2018-09-12 10:25:02 +0200

    add system health endpoint that returns 200 and is empty

---
+++ src/Blaster.Tests/Features/System/TestSystemRoutes.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Blaster.Tests.Builders;
+using Xunit;
+
+namespace Blaster.Tests.Features.System
+{
+    public class TestSystemRoutes
+    {
+        [Fact]
+        public async Task get_health_returns_expected_status_code()
+        {
+            using (var clientBuilder = new HttpClientBuilder())
+            {
+                var client = clientBuilder.Build();
+
+                var response = await client.GetAsync("/system/health");
+                
+                Assert.Equal(
+                    expected: HttpStatusCode.OK,
+                    actual: response.StatusCode
+                );
+            }
+        }
+    }
+}
---
+++ src/Blaster.WebApi/Features/System/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blaster.WebApi.Features.System
+{
+    [Route("system")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [Route("health")]
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "";
+        }
+    }
+}
