71c2ec3f876aba4a56db53f0d2e4df9c1c6c0056
kim <kilin@dfds.com>
2018-09-12 06:15:27 +0200

    add cognito webapi

---
+++ src/Cognito.WebApi/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cognito.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult<string> Get(int id)
+        {
+            return "hello world";
+        }
+    }
+}
