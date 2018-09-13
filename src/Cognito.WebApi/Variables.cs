a7a1914a65d6a786da1334fc0841d2b1b9db1042
Heidelbach Jacob Friis <jfhei@DFDS.COM>
2018-09-13 12:36:41 +0200

    Added api contracts for aws console link generation

--- src/Cognito.WebApi/Variables.cs
+++ src/Cognito.WebApi/Variables.cs
@@ -6,24 +6,21 @@ namespace Cognito.WebApi
 {
     public class Variables
     {
-        private const string AwsAccessKeyEnvironmentVariable = "aws-cognito-access-key"; 
+        private const string AwsAccessKeyEnvironmentVariable = "aws-cognito-access-key";
+        private const string AwsCognitoSecretAccessKey = "aws-cognito-secret-access-key";
         public string AwsAccessKey { get; }
         public string AwsSecretAccessKey { get; }
 
         public Variables()
         {
             AwsAccessKey = Environment.GetEnvironmentVariable(AwsAccessKeyEnvironmentVariable);
-            AwsSecretAccessKey = Environment.GetEnvironmentVariable("aws-cognito-secret-access-key");
+            AwsSecretAccessKey = Environment.GetEnvironmentVariable(AwsCognitoSecretAccessKey);
         }
 
         public void Validate()
         {
             var errors = new List<string>();
-            
-            
             if(errors.Any() == false) {return;}
-            
-            
         }
     }
 }
