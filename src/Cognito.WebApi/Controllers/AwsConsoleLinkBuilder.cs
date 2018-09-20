8b65f1744264df33db32c89545d393dd27cbf66f
Kim Lindhard <kilin@dfds.com>
2018-09-20 08:12:50 +0200

    move AwsConsoleService back into AwsConsoleLinkBuilder

--- src/Cognito.WebApi/Controllers/AwsConsoleLinkBuilder.cs
+++ src/Cognito.WebApi/Controllers/AwsConsoleLinkBuilder.cs
@@ -7,7 +7,6 @@ using Amazon;
 using Amazon.CognitoIdentity;
 using Amazon.SecurityToken;
 using Amazon.SecurityToken.Model;
-using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
@@ -15,20 +14,58 @@ namespace Cognito.WebApi.Controllers
 {
     public class AwsConsoleLinkBuilder : IAwsConsoleLinkBuilder
     {
-        public async Task<Uri> GenerateUriForConsole(string idToken)
+        /// <summary>Creates a AWS console login uri</summary>
+        /// <param name="identityToken">The token provided by the identity provider.</param>
+        public async Task<Uri> GenerateUriForConsole(string identityToken)
         {
-          var cred = new CognitoAWSCredentials("eu-central-1:07c2b8e5-42a1-4791-a006-ea4e14611de8", RegionEndpoint.EUCentral1);
-            cred.AddLogin("cognito-idp.eu-central-1.amazonaws.com/eu-central-1_y2kIM3LEL", idToken);
+            var identityPoolId = "eu-central-1:07c2b8e5-42a1-4791-a006-ea4e14611de8";
+            var providerName = "cognito-idp.eu-central-1.amazonaws.com/eu-central-1_y2kIM3LEL";
+            var roleArn = "arn:aws:iam::528563840976:role/Cognito_BlasterAuth_Role";
 
-            var client = new AmazonSecurityTokenServiceClient(cred);
-            var assumedRole = await client.AssumeRoleAsync(new AssumeRoleRequest
-            {
-                //RoleArn = "arn:aws:iam::515815634748:role/Cognito_FutteIdentityAuth1_Role",
-                RoleArn = "arn:aws:iam::528563840976:role/Cognito_BlasterAuth_Role",
-                RoleSessionName = "AssumeRoleSession"
-            });
+            var credentialsPayload = await AssumeRole(
+                identityPoolId,
+                providerName,
+                roleArn,
+                identityToken
+            );
 
-            var payload = new CredentialsPayload
+            var signinToken = await GetSignInToken(credentialsPayload);
+
+            var consoleLoginUri = BuildConsoleLoginUri(signinToken);
+
+
+            return consoleLoginUri;
+        }
+
+
+        public async Task<CredentialsPayload> AssumeRole(
+            string identityPoolId,
+            string providerName,
+            string roleArn,
+            string identityToken
+        )
+        {
+            var regionEndpoint = RegionEndpoint.GetBySystemName(identityPoolId.Split(':')[0]);
+
+            var cognitoAwsCredentials = new CognitoAWSCredentials(
+                identityPoolId,
+                regionEndpoint
+            );
+
+            cognitoAwsCredentials.AddLogin(
+                providerName,
+                identityToken
+            );
+
+            var securityTokenServiceClient = new AmazonSecurityTokenServiceClient(cognitoAwsCredentials);
+            var assumedRole = await securityTokenServiceClient.AssumeRoleAsync(new AssumeRoleRequest
+                {
+                    RoleArn = roleArn,
+                    RoleSessionName = "AssumeRoleSession"
+                }
+            );
+
+            var credentialsPayload = new CredentialsPayload
             {
                 SessionId = assumedRole.Credentials.AccessKeyId,
                 SessionKey = assumedRole.Credentials.SecretAccessKey,
@@ -36,43 +73,56 @@ namespace Cognito.WebApi.Controllers
             };
 
 
+            return credentialsPayload;
+        }
+
+
+        public Uri BuildConsoleLoginUri(string signinToken)
+        {
             var sb = new StringBuilder();
-            sb.Append("?Action=getSigninToken");
-            //sb.Append("&SessionDuration=43200");
-            sb.Append("&Session=");
-
-            DefaultContractResolver contractResolver = new DefaultContractResolver
-            {
-                NamingStrategy = new CamelCaseNamingStrategy()
-            };
-
-
-            var jsonPart = WebUtility.UrlEncode(JsonConvert.SerializeObject(payload, new JsonSerializerSettings
-            {
-                ContractResolver = contractResolver
-            }));
-
-            sb.Append(jsonPart);
-
-            var httpClient = new HttpClient();
-            var uri = new Uri("https://signin.aws.amazon.com/federation" + sb);
-            var result = await httpClient.GetAsync(uri);
-
-            if (!result.IsSuccessStatusCode)
-                throw new ApplicationException("Failed to create signintoken");
-                    
-            var content = await result.Content.ReadAsStringAsync();
-            var token = JsonConvert.DeserializeObject<Token>(content);
-
-            sb.Clear();
+            sb.Append("https://signin.aws.amazon.com/federation");
             sb.Append("?Action=login");
             sb.Append("&Issuer=app.dfds.cloud");
             sb.Append("&Destination=" + WebUtility.UrlEncode("https://console.aws.amazon.com/"));
-            sb.Append("&SigninToken=" + token.SigninToken);
-            uri = new Uri("https://signin.aws.amazon.com/federation" + sb);
-            return uri;
+            sb.Append("&SigninToken=" + signinToken);
+
+            var consoleLoginUri = new Uri(sb.ToString());
+
+
+            return consoleLoginUri;
+        }
+
+
+        public async Task<string> GetSignInToken(CredentialsPayload credentialsPayload)
+        {
+            var contractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            };
+            var session = WebUtility.UrlEncode(JsonConvert.SerializeObject(
+                credentialsPayload,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = contractResolver
+                }
+            ));
+
+            var httpClient = new HttpClient();
+
+            var uri = new Uri($"https://signin.aws.amazon.com/federation?Action=getSigninToken&Session={session}");
+            var httpResponse = await httpClient.GetAsync(uri);
+
+            if (!httpResponse.IsSuccessStatusCode)
+                throw new ApplicationException("Failed to create signintoken");
+
+            var content = await httpResponse.Content.ReadAsStringAsync();
+            var token = JsonConvert.DeserializeObject<Token>(content);
+
+
+            return token.SigninToken;
         }
     }
+
 
     public class CredentialsPayload
     {
