b2df6974281ae959a19a45c3d29f9241189b67c2
Kim Lindhard <kilin@dfds.com>
2018-10-29 14:41:55 +0100

    CognitoService uses team service api url

--- src/Blaster.WebApi/Features/System/CognitoService.cs
+++ src/Blaster.WebApi/Features/System/CognitoService.cs
@@ -10,29 +10,28 @@ namespace Blaster.WebApi.Features.System
 {
     public class CognitoService : ICognitoService
     {
-        private const string CognitoApiUrlKey = "BLASTER_COGNITO_API_URL";
+        private const string TeamServiceApiUrlKey = "BLASTER_TEAMSERVICE_API_URL";
 
         private readonly HttpClient _client;
         private readonly IJsonSerializer _serializer;
-        private readonly string _cognitoApiUrl;
+        private readonly string _teamApiUrl;
 
         public CognitoService(IConfiguration configuration, HttpClient client, IJsonSerializer serializer)
         {
-            _cognitoApiUrl = configuration[CognitoApiUrlKey];
+            _teamApiUrl = configuration[TeamServiceApiUrlKey];
 
-            if (string.IsNullOrWhiteSpace(_cognitoApiUrl))
+            if (string.IsNullOrWhiteSpace(_teamApiUrl))
             {
-                throw new MissingConfigurationException($"Error, missing configuration value for \"{CognitoApiUrlKey}\".");
+                throw new MissingConfigurationException($"Error, missing configuration value for \"{TeamServiceApiUrlKey}\".");
             }
 
             _client = client;
             _serializer = serializer;
         }
-//         [HttpGet("api/teams/{id}/aws/console-url")]
 
         public async Task<string> SayHello()
         {
-            var response = await _client.GetAsync($"{_cognitoApiUrl}/system/health");
+            var response = await _client.GetAsync($"{_teamApiUrl}/system/health");
             response.EnsureSuccessStatusCode();
 
             return await response.Content.ReadAsStringAsync();
@@ -40,7 +39,7 @@ namespace Blaster.WebApi.Features.System
 
         public async Task<AwsConsoleLinkResponse> GetAwsConsoleLink(Guid teamId, string idToken)
         {
-            var response = await _client.GetAsync($"{_cognitoApiUrl}/api/teams/{teamId}/aws/console-url?idToken={idToken}");
+            var response = await _client.GetAsync($"{_teamApiUrl}/api/teams/{teamId}/aws/console-url?idToken={idToken}");
             var content = await response.Content.ReadAsStringAsync();
 
             return _serializer.Deserialize<AwsConsoleLinkResponse>(content);
