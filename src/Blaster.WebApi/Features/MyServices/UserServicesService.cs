be78eb21f7052272acf4fddd3c597c58d2ce5254
Kim Lindhard <kilin@dfds.com>
2018-10-30 07:49:07 +0100

    add test for UserServicesService ReBaseServiceLocations

--- src/Blaster.WebApi/Features/MyServices/UserServicesService.cs
+++ src/Blaster.WebApi/Features/MyServices/UserServicesService.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Blaster.WebApi.Features.MyServices
 {
-    public class UserServicesService :IUserServicesService
+    public class UserServicesService : IUserServicesService
     {
         private const string BlasterUrlKey = "BLASTER_URL";
         private const string TeamServiceApiUrlKey = "BLASTER_TEAMSERVICE_API_URL";
@@ -27,18 +27,18 @@ namespace Blaster.WebApi.Features.MyServices
         {
             _blasterBaseUri = GetStringFromConfiguration(configuration, BlasterUrlKey);
             _teamsBaseUri = GetStringFromConfiguration(configuration, TeamServiceApiUrlKey);
-       
+
             _client = client;
             _serializer = serializer;
         }
 
-        
+
         private string GetStringFromConfiguration(
-            IConfiguration configuration, 
+            IConfiguration configuration,
             string configurationKey
         )
         {
-            var uriString = configuration[configurationKey]; 
+            var uriString = configuration[configurationKey];
             if (string.IsNullOrWhiteSpace(uriString))
             {
                 throw new MissingConfigurationException(
@@ -48,8 +48,31 @@ namespace Blaster.WebApi.Features.MyServices
             return uriString;
         }
 
-        
+
         public async Task<TeamsDTO> GetServices(string userId)
+        {
+            var teams = await GetServicesFromTeamsService(userId);
+
+            teams = ReBaseServiceLocations(_blasterBaseUri, teams);
+
+
+            return teams;
+        }
+
+
+        public static TeamsDTO ReBaseServiceLocations(string baseUri, TeamsDTO teams)
+        {
+            foreach (var service in teams.Items.SelectMany(t => t.Services))
+            {
+                service.Location = baseUri.AddPath(service.Location);
+            }
+
+            
+            return teams;
+        }
+
+
+        public async Task<TeamsDTO> GetServicesFromTeamsService(string userId)
         {
             var teamsServicesUri = _teamsBaseUri.AddPath(
                 $"api/users/{userId}/services"
@@ -59,11 +82,6 @@ namespace Blaster.WebApi.Features.MyServices
             var content = await response.Content.ReadAsStringAsync();
 
             var teams = _serializer.Deserialize<TeamsDTO>(content);
-
-            foreach (var service in teams.Items.SelectMany(t => t.Services))
-            {
-                service.Location =_blasterBaseUri.AddPath(service.Location);
-            }
 
 
             return teams;
