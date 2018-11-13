fb5da5bc04049f6bc60bb2e54d231265a2394865
Kim Lindhard <kilin@dfds.com>
2018-11-13 17:53:23 +0100

    add loading text to myServices

--- src/Blaster.WebApi/Features/MyServices/app.js
+++ src/Blaster.WebApi/Features/MyServices/app.js
@@ -7,6 +7,7 @@ const myServicesClient = new MyServicesClient();
 const app = new Vue({
     el: '#myservices-app',
     data: {
+        initializing: true,
         teams: [],
         currentUser: currentUser
     },
@@ -21,6 +22,7 @@ const app = new Vue({
             .then(data => {
                 const items = data.items || [];
                 app.teams = items;
-            });
+            })
+            .then(() => app.initializing = false);
     }
 });
