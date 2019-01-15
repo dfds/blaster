83f7afaee15147ca46f3bcffdc79a8692c2d9ff6
Jakob Nielsen <jakob.nielsen@dfds.com>
2019-01-15 14:25:00 +0100

    Added pipeline script for building auth-proxy.

---
+++ pipeline-auth-proxy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+[[ -n $DEBUG ]] && set -x
+set -eu -o pipefail
+
+# build parameters
+readonly REGION=${AWS_DEFAULT_REGION:-"eu-central-1"}
+readonly IMAGE_NAME='auth-proxy'
+readonly BUILD_NUMBER=${1:-"N/A"}
+
+restore_dependencies() {
+    echo "Restoring dependencies"
+    npm install
+}
+
+build_container_image() {
+    echo "Building container image..."
+    docker build -t ${IMAGE_NAME} .
+}
+
+push_container_image() {
+    echo "Login to docker..."
+    $(aws ecr get-login --no-include-email)
+
+    account_id=$(aws sts get-caller-identity --output text --query 'Account')
+    image_name="${account_id}.dkr.ecr.${REGION}.amazonaws.com/ded/${IMAGE_NAME}:${BUILD_NUMBER}"
+
+    echo "Tagging container image..."
+    docker tag ${IMAGE_NAME}:latest ${image_name}
+
+    echo "Pushing container image to ECR..."
+    docker push ${image_name}
+}
+
+cd ./src/auth-proxy
+
+restore_dependencies
+build_container_image
+
+cd ../../
+
+if [[ "${BUILD_NUMBER}" != "N/A" ]]; then
+    push_container_image
+fi
