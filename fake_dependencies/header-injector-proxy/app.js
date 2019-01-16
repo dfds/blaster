2181abf08c438e2dcb6b6768d3005e5361bcad28
Jakob Nielsen <jakob.nielsen@dfds.com>
2019-01-16 18:08:54 +0100

    Added a header injector proxy to ease local development.

---
+++ fake_dependencies/header-injector-proxy/app.js
@@ -0,0 +1,18 @@
+const proxy = require('express-http-proxy');
+const app = require('express')();
+const port = process.env.PORT || 50802;
+const forwardAddress = process.env.FORWARD_ADDRESS || "localhost:50800";
+
+app.use('/', proxy(forwardAddress, {
+    proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+        proxyReqOpts.headers['X-User-Id'] = "jane@doe.com";
+        proxyReqOpts.headers['X-User-Name'] = "Jane Doe";
+        proxyReqOpts.headers['X-User-Email'] = "jane@doe.com";
+
+        return proxyReqOpts;
+    }})
+);
+
+app.listen(port, () => {
+    console.log(`header-injector-proxy is running on port ${port}`);
+});
