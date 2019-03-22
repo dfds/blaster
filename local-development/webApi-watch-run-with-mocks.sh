14bc37dcd03372a1c1cb95517e6a6f0cf96f828b
Kim Lindhard <kilin@dfds.com>
2019-03-22 14:19:46 +0100

    add BLASTER_AWS_JANITOR_API_URL to webApi-watch-run-with-mocks.sh

--- local-development/webApi-watch-run-with-mocks.sh
+++ local-development/webApi-watch-run-with-mocks.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-(cd ../src/Blaster.WebApi/ &&  BLASTER_CAPABILITYSERVICE_API_URL=http://localhost:50900 BLASTER_IAMROLESERVICE_API_URL=http://localhost:50901 dotnet watch run)
+BLASTER_CAPABILITYSERVICE_API_URL=http://localhost:50900 \
+BLASTER_IAMROLESERVICE_API_URL=http://localhost:50901 \
+BLASTER_AWS_JANITOR_API_URL=http://localhost:50902 \
+dotnet watch --project ../src/Blaster.WebApi/ run
