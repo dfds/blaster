8b18d8145e4d97461238d65249adb0ec5c1026ef
Emil H. Clausen <emcla@dfds.com>
2019-08-20 11:23:21 +0200

    Updated test filter

--- src/Blaster.WebApi/Features/Capabilities/test-filter.js
+++ src/Blaster.WebApi/Features/Capabilities/test-filter.js
@@ -1,7 +1,8 @@
 const FilteredCapabilities = new Set(["926458e2-4468-4bca-97b0-520584ccc0f4", "4e01a646-9a0f-4c33-905e-2abef8d96730", "78c4e6e2-127c-4cbd-b0e2-0dd8dfcf5fbc", "5cb28c0e-87a2-43ed-a3b2-bedbb02d5757",
                                         "c3b3f403-d259-42de-83b0-604874808d0e", "1ca9fd66-aa1b-46f9-9657-68f26657478c", "a1fc255f-cfc4-46b4-9831-82a239e088bb", "c56a4853-ec73-48ee-8ab4-64ffc56d5586", 
                                         "c73e9e9e-a9ac-430e-baee-b08b97c6a58b", "4ed8f786-c700-4562-80a8-27e444215853", "bcf07274-2ac1-44e7-aeb9-fadba80c97db", "826a0440-af08-4721-8c14-2db8a3f127d0",
-                                        "c99915d9-b635-44b5-a04c-58e6a943b810", "84831ab1-9432-4640-b4a1-a4f0c6d2202f", "8efcd4fe-4b69-4a28-9993-ed5f0dd094ff", "2f3a3304-356f-408b-a64a-afdfc6792409"]);
+                                        "c99915d9-b635-44b5-a04c-58e6a943b810", "84831ab1-9432-4640-b4a1-a4f0c6d2202f", "8efcd4fe-4b69-4a28-9993-ed5f0dd094ff", "2f3a3304-356f-408b-a64a-afdfc6792409",
+					"3741b4e1-8c1f-4495-83a6-91b2279599d6"]);
 
 export {FilteredCapabilities};
 export default FilteredCapabilities;
