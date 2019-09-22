fb9ee9b0812eb485be9df23118422c62b01d5a10
Kim Lindhard <kilin@dfds.com>
2019-09-22 07:26:10 +0200

    connection shows channel name, sender name

--- src/Blaster.WebApi/Features/CommunicationChannels/Models/Connection.cs
+++ src/Blaster.WebApi/Features/CommunicationChannels/Models/Connection.cs
@@ -4,7 +4,9 @@ namespace Blaster.WebApi.Features.CommunicationChannels.Models
     {
         public string SenderType { get; set; }
         public string SenderId { get; set; }
+        public string SenderName { get; set; }
         public string ChannelType { get; set; }
+        public string ChannelName { get; set; }
         public string ChannelId { get; set; }
     }
 }
