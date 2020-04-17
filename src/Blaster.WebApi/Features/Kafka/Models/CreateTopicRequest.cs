d08c769d4f9c27367bc161aef281462abaa25b29
Emil H. Clausen <sequoiia@hummel.yt>
2020-04-17 13:58:03 +0200

    Removed dead property

--- src/Blaster.WebApi/Features/Kafka/Models/CreateTopicRequest.cs
+++ src/Blaster.WebApi/Features/Kafka/Models/CreateTopicRequest.cs
@@ -8,7 +8,6 @@ namespace Blaster.WebApi.Features.Capabilities.Models
 		public string Name { get; set; }
 		public string Description { get; set; }
 		public int Partitions { get; set; }
-		public long RetentionPeriodInDays { get; set; }
 		public bool DryRun { get; set; }
 		public Dictionary<string, object> Configurations { get; set; }
 	}
